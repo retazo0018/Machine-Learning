4e4ec9146dfd247765819b3dffb71b80a3f71aef
Ashwin Murali <ashwin.cse18@gmail.com>
2019-06-07 15:01:05 +0530

    Create Classification-template.py

---
+++ Classification-template.py
@@ -0,0 +1,71 @@
+# Classification Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, []].values #independent variables
+y = dataset.iloc[:, ].values #dependent variables
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+# scaling x-min(x)//max(x)-min(x)
+# scalinf x-mean(x)//standard deviation(x)
+
+#Fitting classifier to test set 
+#create a classifier here 
+#predicting test results
+y_pred = classifier.predict(X_test)
+
+#Making the confusion matrix (No. of correct predictions and No. of incorrect predicitons)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred) #65 and 24 are correct predictions and 8 and 3 are incorrect predictions
+
+#Visualising the training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step=0.01), 
+                     np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
+plt.contourf(X1,X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                                       alpha = 0.75, cmap = ListedColormap(('red' , 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set ==j, 1],
+                c = ListedColormap(('red','green'))(i),label = j)
+plt.title('Classifier (Training set)')
+plt.xlabel('')
+plt.ylabel('')
+plt.legend()
+plt.show()
+
+#Visualising the test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step=0.01), 
+                     np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
+plt.contourf(X1,X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                                       alpha = 0.75, cmap = ListedColormap(('red' , 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set ==j, 1],
+                c = ListedColormap(('red','green'))(i),label = j)
+plt.title('Classifier (Training set)')
+plt.xlabel('')
+plt.ylabel('')
+plt.legend()
+plt.show()
